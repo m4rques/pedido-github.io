777f576123a729a235ce68933b974d2a952d7bfc
Kauan Marques <128805665+m4rques@users.noreply.github.com>
2023-12-05 22:19:29 -0300

    Add files via upload

---
+++ pedido de namoro/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('sim').addEventListener('click', function() {
+  window.location.href = 'https://open.spotify.com/intl-pt/track/15MJ5NThPjj6xhPcts8MiY?si=2a74ff0c8e4f4ac1'; 
+});
+
+document.getElementById('nao').addEventListener('click', function() {
+  alert('Ah, que pena! Quem sabe na próxima?');
+});
